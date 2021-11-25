2fc158c505cfbe1c400c02a1cfdac3598f877372
Sam <85004512+aar2dee2@users.noreply.github.com>
2021-11-25 10:36:03 +0000

    added WrongLive.handle_event/3 function, using ~H instead of ~L in WrongLive.render/1

--- pento/lib/pento_web/live/wrong_live.ex
+++ pento/lib/pento_web/live/wrong_live.ex
@@ -1,6 +1,20 @@
 defmodule PentoWeb.WrongLive do
   use PentoWeb, :live_view
 
+  def handle_event("guess", %{"number" => guess} = data, socket) do
+    IO.inspect data
+    message = "Your guess is: #{guess}. Guess again."
+    score = socket.assigns.score - 1
+    {
+      :noreply, 
+      assign(
+        socket,
+        message: message,
+        score: score
+      )
+    }
+  end
+
   def mount(_params, _session, socket) do
     {
       :ok,
@@ -13,7 +27,8 @@ defmodule PentoWeb.WrongLive do
   end
 
   def render(assigns) do
-    ~L"""
+    #The book uses the ~L sigil on page 20 for rendering in LEEx, however, this app uses phoenix 1.6 which uses HEEx, hence the ~H sigil has been used
+    ~H"""
     <h1>
       Your score: <%= @score %>
     </h1>
@@ -24,7 +39,7 @@ defmodule PentoWeb.WrongLive do
 
     <h2>
       <%= for n <- 1..10 do%>
-        <a href="#" phx-click="guess" phx-value-number={n}>
+        <a href="#" phx-click="guess" phx-value-number={n} >
           <%= n %>
         </a>
       <% end %>
